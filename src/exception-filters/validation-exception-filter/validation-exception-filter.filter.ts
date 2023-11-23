f7264ba09522826ab5c300002dc7b1f4d5c5d6c8
zaidkatbeh <72863351+zaidkatbeh@users.noreply.github.com>
2023-11-23 13:56:05 +0200

    updates

--- src/exception-filters/validation-exception-filter/validation-exception-filter.filter.ts
+++ src/exception-filters/validation-exception-filter/validation-exception-filter.filter.ts
@@ -1,8 +1,23 @@
-import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
+import { ArgumentsHost, Catch, ExceptionFilter, HttpException, UnauthorizedException } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Catch()
 export class ValidationExceptionFilterFilter<T> implements ExceptionFilter {
-  catch(exception: T, host: ArgumentsHost) {
-    
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response: Response = ctx.getResponse<Response>();
+    const request: Request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+
+    if(exception instanceof HttpException) {
+      console.log("this is an unautharized exception");
+    }    
+    response
+    .status(status)
+    .json({
+      statusCode: status,
+      
+    });
+
   }
 }
